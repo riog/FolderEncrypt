b9e43bcb5c64baa311e56abc0b69cc3a1cf8fa3e
Rio Gesulgon <r_gesulgon@yahoo.com>
2012-03-09 18:25:01 +0800

    Test class

---
+++ src/test/java/persistence/FileStoreDBTest.java
@@ -0,0 +1,13 @@
+package persistence;
+
+import java.util.Map;
+
+public class FileStoreDBTest {
+	public static void main(String[] args) {
+		Map<Long, String> fileStore = FileStoreDB.getInstance().getFileStore();
+		for(Long key: fileStore.keySet()) {
+			System.out.println(key + ": " + fileStore.get(key));
+			
+		}
+	}
+}
